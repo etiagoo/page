732667666ab91955fa2d747dd6a25835a7288227
etiagoo <141789295+etiagoo@users.noreply.github.com>
2023-08-11 19:53:36 -0300

    Add files via upload

---
+++ oiii.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const contactForm = document.getElementById('contact-form');
+
+    contactForm.addEventListener('submit', function (e) {
+        e.preventDefault();
+
+        // Realize as validações do formulário aqui
+        // Exemplo: verificar se o e-mail é válido
+
+        // Em seguida, você pode enviar os dados do formulário para um servidor ou realizar outras ações
+    });
+});
